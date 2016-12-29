039e9e7f616cbb35de443ce41057700114ce1a6f
Scott MacWatters <code@macwatters.com>
2016-12-29 00:38:58 -0600

    Fixing Issue with new metric

--- summary-utils.js
+++ summary-utils.js
@@ -16,7 +16,7 @@
   };
 
   module.exports.getRecordAmount = function(timesForDc){
-    var numRecords = Object.keys(timesForDc);
+    var numRecords = Object.keys(timesForDc).length;
     var status = 'good';
     //if less than 5 days
     if(numRecords < (24 * 6 * 5)){
