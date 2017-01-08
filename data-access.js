157268bf08f41210708257099d253e5bc6372357
Scott MacWatters <code@macwatters.com>
2017-01-07 23:35:40 -0600

    Adding some namespacing to db for some data separation.

--- data-access.js
+++ data-access.js
@@ -1,7 +1,10 @@
 (function() {
 
-
   var fb_admin = require("firebase-admin");
+
+  //String to differentiate from testing and prod data.
+  //Should probably set up a new project for local use but ... eh.
+  var testingNamespace = process.env.firebase_testing_namespace;
 
   var firebase_config = {
     "type": "service_account",
@@ -23,6 +26,8 @@
     databaseURL: firebase_database_url
   });
   var db = fb_admin.database();
+
+  var deployRefName = (testingNamespace ? testingNamespace : '') + 'deploy';
 
   //Note: None of these things are encrypted in the DB.
   //These are essentially throw away orgs with no production
@@ -46,26 +51,26 @@
 
   module.exports.saveDeployTime = function(datacenter, deployTimes){
     var id = deployTimes.deployId;
-    var dcDeployTimeRef = db.ref('deploy/' + datacenter + '/' + id + '/');
+    var dcDeployTimeRef = db.ref(deployRefName + '/' + datacenter + '/' + id + '/');
     dcDeployTimeRef.set(deployTimes);
   }
 
   module.exports.getDeployTimes = function(callback){
-    var deployTimesRef = db.ref('deploy/');
+    var deployTimesRef = db.ref(deployRefName + '/');
     deployTimesRef.once('value').then(function(snapshot){
       callback(snapshot.val());
     });
   };
 
   module.exports.getDeployTimesForDatacenterForDates = function(datacenter, startTime, endTime, callback){
-    var deployDcRef = db.ref('deploy/' + datacenter + '/');
+    var deployDcRef = db.ref(deployRefName + '/' + datacenter + '/');
     deployDcRef.orderByChild('createdDate').startAt(startTime).endAt(endTime).once('value').then(function(snapshot){
       callback(snapshot.val());
     });
   }
 
   module.exports.getDeployTimesForDatacenter = function(datacenter, callback){
-    var dcDeployTimesRef = db.ref('deploy/' + datacenter + '/');
+    var dcDeployTimesRef = db.ref(deployRefName + '/' + datacenter + '/');
     dcDeployTimesRef.once('value').then(function(snapshot){
       callback(snapshot.val());
     });
@@ -83,12 +88,12 @@
   };
 
   module.exports.saveDeployRequest = function(datacenter, deployRequestId){
-    var dcDeployRequestRef = db.ref('deploy-request/' + datacenter + '/');
+    var dcDeployRequestRef = db.ref(deployRefName + '-request/' + datacenter + '/');
     dcDeployRequestRef.push({'asyncProcessId':deployRequestId});
   }
 
   module.exports.clearCompletedDeployRequest = function(datacenter, deployRequestId){
-    var dcDeployRequestRef = db.ref('deploy-request/' + datacenter + '/');
+    var dcDeployRequestRef = db.ref(deployRefName + '-request/' + datacenter + '/');
 
     dcDeployRequestRef.orderByChild('asyncProcessId').equalTo(deployRequestId).once('value').then(function(snapshot) {
       snapshot.forEach(function(child){
@@ -98,7 +103,7 @@
   }
 
   module.exports.getDeployRequests = function(datacenter, callback){
-    var dcDeployRequestRef = db.ref('deploy-request/' + datacenter + '/');
+    var dcDeployRequestRef = db.ref(deployRefName + '-request/' + datacenter + '/');
     dcDeployRequestRef.once('value').then(function(snapshot){
       callback(snapshot.val());
     });
